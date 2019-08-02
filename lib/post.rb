5832d82599df1b00f70d9fe4d109e41e4e989eb7
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-08-02 19:16:20 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,24 +1,4 @@
 class Post 
   attr_accessor :name, :author, :title
-  
-  #array of all the songs:
-  @@all = []
-  
-  def initialize(title) 
-    @title = title 
-    # @name = name 
-    @author = author  
-    @@all << self 
-  end
-  
-  def self.all
-    @@all
-  end
-  
-  def author_name 
-    title.author = self  
-  end
- 
-  
 end
 
